0a21262910f172d819519b4be6a4ee0a07db3f21
ZakharZabolotskikh <zabolotskikhpnz@gmail.com>
2024-11-12 19:52:40 +0300

    Update Authenticator.cpp

--- Authenticator.cpp
+++ Authenticator.cpp
@@ -1,22 +1,6 @@
-// Authenticator.cpp
 #include "Authenticator.h"
 #include <cstring>
 #include <unistd.h>
-#include <openssl/sha.h>
-#include <sstream>
-#include <iomanip>
-
-std::string generate_hash(const std::string &salt) {
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256((unsigned char*)salt.c_str(), salt.length(), hash);
-
-    // Преобразование хэша в шестнадцатеричную строку
-    std::ostringstream oss;
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
-        oss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
-    }
-    return oss.str();
-}
 
 void Authenticator::authenticate(int socket) {
     std::string salt16 = compute_salt();
