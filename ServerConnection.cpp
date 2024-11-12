494e216bea77add55a408d14634f0acaa5f57a8f
ZakharZabolotskikh <zabolotskikhpnz@gmail.com>
2024-11-12 18:52:02 +0300

    Create ServerConnection.cpp

---
+++ ServerConnection.cpp
@@ -0,0 +1,35 @@
+#include "ServerConnection.h"
+#include <cstring>
+#include <unistd.h>
+#include <iostream>
+#include <arpa/inet.h>
+
+ServerConnection::ServerConnection(const std::string& ip, uint64_t port)
+    : ip(ip), port(port) {
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+}
+
+void ServerConnection::connect() {
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &server_addr.sin_addr);
+
+    if (::connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        throw std::runtime_error("Connection failed");
+    }
+}
+
+void ServerConnection::send_vectors(const std::vector<Vector>& vectors) {
+    uint32_t count = vectors.size();
+    send(sock, &count, sizeof(uint32_t), 0);  // Send count
+
+    for (const auto& vector : vectors) {
+        send(sock, &vector.size, sizeof(uint32_t), 0);  // Send size
+        send(sock, vector.values.data(), vector.size * sizeof(uint64_t), 0);  // Send values
+    }
+}
+
+void ServerConnection::close() {
+    ::close(sock);
+}
