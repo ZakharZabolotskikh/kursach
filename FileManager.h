e9c5a4cc85220954d895893bf9cdf9949f1ef8e3
ZakharZabolotskikh <zabolotskikhpnz@gmail.com>
2024-12-15 15:53:33 +0300

    Update FileManager.h

--- FileManager.h
+++ FileManager.h
@@ -13,9 +13,24 @@
 #include <cstdint>
 #include "Vector.h"
 
+/**
+ * @class FileManager
+ * @brief A utility class for handling file operations related to vectors and results.
+ */
 class FileManager {
 public:
+    /**
+     * @brief Reads vectors from a specified file.
+     * @param filename The name of the file to read from.
+     * @return A vector of Vector objects read from the file.
+     */
     static std::vector<Vector> read_vectors(const std::string& filename);
+
+    /**
+     * @brief Writes results to a specified file.
+     * @param filename The name of the file to write to.
+     * @param results A vector of uint64_t results to be written to the file.
+     */
     static void write_results(const std::string& filename, const std::vector<uint64_t>& results);
 };
 
